ea5cf5fe84ae344c7a69c5d2340ae761859e36fb
in0ho1no <dummy>
2024-07-09 21:41:54 +0900

    ・同一アドレスは出力しないように変更 ・停止条件を、5文字以上のデバイス名1台から、10台に変更

--- main.py
+++ main.py
@@ -4,16 +4,25 @@ from bleak import BleakScanner
 
 
 async def main() -> None:
+    bd_adrs_list: list[str] = []
+
     async with BleakScanner() as scanner:
         print("Scanning...")
 
         n = 5
+        cnt = 10
         print(f"\nFind device with name longer than {n} characters...")
         async for bd, ad in scanner.advertisement_data():
-            found = len(bd.name or "") > n or len(ad.local_name or "") > n
-            print(f" Found{' it' if found else ''} {bd!r} with {ad!r}")
-            if found:
-                break
+            if bd.address not in bd_adrs_list:
+                bd_adrs_list.append(bd.address)
+
+                found = len(bd.name or "") > n or len(ad.local_name or "") > n
+                print(f" Found{' it' if found else ''} {bd!r}")
+                if found:
+                    cnt -= 1
+
+                if 0 == cnt:
+                    break
 
 
 if __name__ == "__main__":
