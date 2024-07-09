d689f2332168f0d043958f80f865724c6c025e53
in0ho1no <dummy>
2024-07-09 21:29:03 +0900

    サンプルを元に5件文字以上のデバイス名が検出されるまでスキャン

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ async def main() -> None:
         print("Scanning...")
 
         n = 5
-        print(f"\n{n} advertisement packets:")
+        print(f"\nFind device with name longer than {n} characters...")
         async for bd, ad in scanner.advertisement_data():
-            print(f" {n}. {bd!r} with {ad!r}")
-            n -= 1
-            if n == 0:
+            found = len(bd.name or "") > n or len(ad.local_name or "") > n
+            print(f" Found{' it' if found else ''} {bd!r} with {ad!r}")
+            if found:
                 break
 
 
