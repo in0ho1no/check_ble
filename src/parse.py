1b6bc04bce7e2abf73966614ad6bf8dbb0d4cae2
in0ho1no <dummy>
2024-07-18 00:33:21 +0900

    タイムスタンプを取得できる状態

--- src/parse.py
+++ src/parse.py
@@ -3,9 +3,9 @@ SRC_FILE_PATH = r"other_in\20240716_2_pc2fpb_2a24-2a00.psd"
 
 class FieldCommon:
     def __init__(self, len_r: int, mean_r: str, data_r: bytes = b"") -> None:
-        self.length = len_r
-        self.mean = mean_r
-        self.data = data_r
+        self.length_m = len_r
+        self.mean_m = mean_r
+        self.data_m = data_r
 
     def get_bytes_data(
         self,
@@ -13,12 +13,11 @@ class FieldCommon:
         len_r: int = 0,
     ) -> bytes:
         if 0 == len_r:
-            len = self.length
+            len = self.length_m
         else:
             len = len_r
 
-        self.data = bytes_data_r[0:len]
-        # print(self.data)
+        self.data_m = bytes_data_r[0:len]
         return bytes_data_r[len:]
 
 
@@ -32,12 +31,20 @@ class FieldNo(FieldCommon):
         super().__init__(len_r, mean_r, data_r)
 
     def get_number(self) -> int:
-        return int.from_bytes(self.data, "little")
+        return int.from_bytes(self.data_m, "little")
 
 
 class FieldTime(FieldCommon):
     def __init__(self, len_r: int, mean_r: str, data_r: bytes = b"") -> None:
         super().__init__(len_r, mean_r, data_r)
+
+    def get_time_us(self) -> int:
+        time_ms = int.from_bytes(self.data_m, "little")
+        time_lo = time_ms & 0xFFFF
+        time_hi = time_ms >> 16
+        time_stamp = time_hi * 5000 + time_lo
+        time_stamp_us = time_stamp / 32
+        return int(time_stamp_us)
 
 
 class FieldLen(FieldCommon):
@@ -45,7 +52,7 @@ class FieldLen(FieldCommon):
         super().__init__(len_r, mean_r, data_r)
 
     def get_length(self) -> int:
-        return int.from_bytes(self.data, "little")
+        return int.from_bytes(self.data_m, "little")
 
 
 class FieldPayload(FieldCommon):
@@ -53,15 +60,25 @@ class FieldPayload(FieldCommon):
         super().__init__(len_r, mean_r, data_r)
 
 
-class PsdData:
+class PacketData:
     def __init__(self, info_r: FieldInfo, no_r: FieldNo, time_r: FieldTime, len_r: FieldLen, pay_r: FieldPayload) -> None:
-        self.pkt_info = info_r
+        self.pkt_info_m = info_r
+        self.pkt_no_m = no_r
+        self.pkt_time_m = time_r
+        self.pkt_len_m = len_r
+        self.pkt_payload_m = pay_r
+
+    def set_timestamp(self, time_us: int) -> None:
+        self.timestamp_m = time_us
+        print(time_us)
 
 
 with open(SRC_FILE_PATH, "rb") as f:
     file_contents = f.read()
 
-    psd_list: list[PsdData] = []
+    cnt = 0
+    base_time_us = 0
+    psd_list: list[PacketData] = []
     while file_contents:
         pkt_info = FieldInfo(1, "Packet_Information")
         pkt_no = FieldNo(4, "Packet_Number")
@@ -74,7 +91,15 @@ with open(SRC_FILE_PATH, "rb") as f:
         file_contents = pkt_time.get_bytes_data(file_contents)
         file_contents = pkt_len.get_bytes_data(file_contents)
         file_contents = pkt_payload.get_bytes_data(file_contents)
+        pkt = PacketData(pkt_info, pkt_no, pkt_time, pkt_len, pkt_payload)
 
-        psd_list.append(PsdData(pkt_info, pkt_no, pkt_time, pkt_len, pkt_payload))
+        # タイムスタンプを正規化する
+        if cnt == 0:
+            base_time_us = pkt_time.get_time_us()
+
+        pkt.set_timestamp(pkt_time.get_time_us() - base_time_us)
+        psd_list.append(pkt)
+
+        cnt += 1
 
     print(len(psd_list))
