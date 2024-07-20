61c9f6c869244ecd9407fabf52b7b268a6af444f
in0ho1no <dummy>
2024-07-20 16:25:06 +0900

    ・結果をファイル出力するように対応

--- src/parse.py
+++ src/parse.py
@@ -4,6 +4,7 @@ import logging
 import parse_PacketData
 
 SRC_FILE_PATH = r"other_in\20240716_2_pc2fpb_2a24-2a00.psd"
+DST_FILE_PATH = r"other_out\output.csv"
 
 
 def main() -> None:
@@ -15,17 +16,19 @@ def main() -> None:
     packet_list = parse_PacketData.get_packet_list(file_contents)
     logging.info(f"Packet Count {len(packet_list)}")
 
-    for packet in packet_list:
-        # 結果出力
-        print(f"\
-{packet.fld_no_m.get_data()},\
-{packet.timestamp_m},\
-{packet.fld_status_bytes_m.channel_m},\
-{hex(packet.fld_payload_m.access_adrs_m)},\
-{packet.fld_status_bytes_m.rssi_m},\
-{packet.fld_status_bytes_m.indicate_crc_m},\
-{packet.fld_payload_m.get_ble_payload_hex()},\
-")
+    # 結果出力
+    with open(DST_FILE_PATH, "w") as f:
+        for packet in packet_list:
+            result = f"\
+    {packet.fld_no_m.get_data()},\
+    {packet.timestamp_m},\
+    {packet.fld_status_bytes_m.channel_m},\
+    {hex(packet.fld_payload_m.access_adrs_m)},\
+    {packet.fld_status_bytes_m.rssi_m},\
+    {packet.fld_status_bytes_m.indicate_crc_m},\
+    {packet.fld_payload_m.get_ble_payload_hex()},\
+    \r\n"
+            f.write(result)
 
 
 # ログ用のstream用意
